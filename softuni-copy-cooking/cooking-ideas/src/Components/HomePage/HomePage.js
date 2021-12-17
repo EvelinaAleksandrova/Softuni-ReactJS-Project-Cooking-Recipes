77812f35214896df467671aab484b37c18947d91
evelina <evety.1dtvd@gmail.com>
2021-12-17 23:53:33 +0200

    Add About Us section in Home Page

--- softuni-copy-cooking/cooking-ideas/src/Components/HomePage/HomePage.js
+++ softuni-copy-cooking/cooking-ideas/src/Components/HomePage/HomePage.js
@@ -29,8 +29,8 @@ const HomePage = () => {
 
             <div className="about-section">
                 <h1>About Us Page</h1>
-                <p>Some text about who we are and what we do.</p>
-                <p>Resize the browser window to see that this page is responsive by the way.</p>
+                <p>We love cooking, eating, and connecting with people from around the world.</p>
+
             </div>
 
             <h2>Our Team</h2>
