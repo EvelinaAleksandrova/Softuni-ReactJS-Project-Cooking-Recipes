660c7f81427d4c17539cf3aa50f14dfa36397230
evelina <evety.1dtvd@gmail.com>
2021-12-23 00:35:45 +0200

    Add new component error boundary

--- softuni-copy-cooking/cooking-ideas/src/App.js
+++ softuni-copy-cooking/cooking-ideas/src/App.js
@@ -21,33 +21,37 @@ import AllRecipesPage from "./Components/AllRecipesPage/AllRecipesPage";
 import DetailsArticle from "./Components/DetailsArticle/DetailsArticle";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import ErrorBoundary from "./Components/Common/ErrorBoundary";
+
 function App() {
     return (
-        <AuthProvider>
-            <div id="container">
-                <Header/>
-                <Routes>
-                    <Route path="/" element={<HomePage/>}/>
-                </Routes>
-                <main id="site-content">
+        <ErrorBoundary>
+            <AuthProvider>
+                <div id="container">
+                    <Header/>
                     <Routes>
-                        <Route path="/home/*" element={<HomePage/>}/>
-                        <Route path="/login" element={<LoginPage/>}/>
-                        <Route path="/logout" element={<LogoutPage/>}/>
-                        <Route path="/register" element={<RegisterPage/>}/>
-                        <Route path="/all-recipes/*" element={<AllRecipesPage/>}/>
-                        <Route path="/all-articles/*" element={<AllArticlesPage/>}/>
-                        <Route path="/my-recipes" element={<MyRecipesPage/>}/>
-                        <Route path="/my-articles" element={<MyArticlesPage/>}/>
-                        <Route path="/create" element={<CreateRecipe/>}/>
-                        <Route path="/create-article" element={<CreateArticle/>}/>
-                        <Route path="/details-recipe/:recipeId" element={<DetailsRecipe/>}/>
-                        <Route path="/details-article/:articleId" element={<DetailsArticle/>}/>
+                        <Route path="/" element={<HomePage/>}/>
                     </Routes>
-                </main>
-                <Footer/>
-            </div>
-        </AuthProvider>
+                    <main id="site-content">
+                        <Routes>
+                            <Route path="/home/*" element={<HomePage/>}/>
+                            <Route path="/login" element={<LoginPage/>}/>
+                            <Route path="/logout" element={<LogoutPage/>}/>
+                            <Route path="/register" element={<RegisterPage/>}/>
+                            <Route path="/all-recipes/*" element={<AllRecipesPage/>}/>
+                            <Route path="/all-articles/*" element={<AllArticlesPage/>}/>
+                            <Route path="/my-recipes" element={<MyRecipesPage/>}/>
+                            <Route path="/my-articles" element={<MyArticlesPage/>}/>
+                            <Route path="/create" element={<CreateRecipe/>}/>
+                            <Route path="/create-article" element={<CreateArticle/>}/>
+                            <Route path="/details-recipe/:recipeId" element={<DetailsRecipe/>}/>
+                            <Route path="/details-article/:articleId" element={<DetailsArticle/>}/>
+                        </Routes>
+                    </main>
+                    <Footer/>
+                </div>
+            </AuthProvider>
+        </ErrorBoundary>
     );
 }
 
