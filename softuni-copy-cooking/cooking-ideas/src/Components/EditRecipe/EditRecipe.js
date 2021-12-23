5916169f59730acbef70295bc1555afb55f67e1e
evelina <evety.1dtvd@gmail.com>
2021-12-23 03:23:01 +0200

    Add new functionality to edit

--- softuni-copy-cooking/cooking-ideas/src/Components/EditRecipe/EditRecipe.js
+++ softuni-copy-cooking/cooking-ideas/src/Components/EditRecipe/EditRecipe.js
@@ -26,24 +26,28 @@ const EditRecipe = () => {
                         <label htmlFor="description">Description</label>
                         <span className="input">
                             <textarea name="description"
-                                      id="description">Today, some dogs are used as pets, others are used to help humans do their work. They are a popular pet because they are usually playful, friendly, loyal and listen to humans. Thirty million dogs in the United States are registered as pets.[5] Dogs eat both meat and vegetables, often mixed together and sold in stores as dog food. Dogs often have jobs, including as police dogs, army dogs, assistance dogs, fire dogs, messenger dogs, hunting dogs, herding dogs, or rescue dogs.</textarea>
+                                      id="description" defaultValue={recipe.description}/>
                         </span>
                     </p>
                     <p className="field">
                         <label htmlFor="image">Image</label>
                         <span className="input">
-                            <input type="text" name="imageUrl" id="image" value="/images/dog.png"/>
+                            <input type="text" name="imageUrl" id="image" defaultValue={recipe.imageUrl}/>
                         </span>
                     </p>
                     <p className="field">
                         <label htmlFor="type">Type</label>
                         <span className="input">
-                            <select id="type" name="type" value="dog">
-                                <option value="cat">Cat</option>
-                                <option value="dog" selected>Dog</option>
-                                <option value="parrot">Parrot</option>
-                                <option value="reptile">Reptile</option>
-                                <option value="other">Other</option>
+                            <select id="type" name="type">
+                                <option value="soup">Soup</option>
+                                <option value="meat">Meat</option>
+                                <option value="vegan">Vegan</option>
+                                <option value="salad">Salad</option>
+                                <option value="dessert">Dessert</option>
+                                <option value="pizza">Pizza</option>
+                                <option value="cocktails">Drinks & Cocktails</option>
+                                <option value="christmas">Christmas</option>
+                                <option value="other-recipe">Other</option>
                             </select>
                         </span>
                     </p>
