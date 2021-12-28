161eb0ce289393a94ecd39a736ee14b77b138df7
evelina <evety.1dtvd@gmail.com>
2021-12-28 13:01:47 +0200

    Change styles and size of components.

--- softuni-copy-cooking/cooking-ideas/src/Components/CommonDirectory/ErrorBoundaries.js
+++ softuni-copy-cooking/cooking-ideas/src/Components/CommonDirectory/ErrorBoundaries.js
@@ -1,5 +1,5 @@
 import {Component} from 'react';
-import "../ErrorPage/ErrorBoundaries.css";
+import "../ErrorPage/ErrorPage.css";
 import ErrorPage from "../ErrorPage/ErrorPage";
 
 class ErrorBoundary extends Component {
--- softuni-copy-cooking/cooking-ideas/src/Components/ErrorPage/ErrorPage.js
+++ softuni-copy-cooking/cooking-ideas/src/Components/ErrorPage/ErrorPage.js
@@ -1,32 +1,32 @@
-const ErrorPage = () =>{
-    return(
+const ErrorPage = () => {
+    return (
         <>
 
-            <div id="background"></div>
-            <div className="top">
-                <h1>404</h1>
-                <h3>page not found</h3>
-            </div>
-            <div className="container">
-                <div className="ghost-copy">
-                    <div className="one"></div>
-                    <div className="two"></div>
-                    <div className="three"></div>
-                    <div className="four"></div>
+                <div id="background"></div>
+                <div className="top">
+                    <h1>404</h1>
+                    <h3>page not found</h3>
                 </div>
-                <div className="ghost">
-                    <div className="face">
-                        <div className="eye"></div>
-                        <div className="eye-right"></div>
-                        <div className="mouth"></div>
+                <div className="container">
+                    <div className="ghost-copy">
+                        <div className="one"></div>
+                        <div className="two"></div>
+                        <div className="three"></div>
+                        <div className="four"></div>
                     </div>
+                    <div className="ghost">
+                        <div className="face">
+                            <div className="eye"></div>
+                            <div className="eye-right"></div>
+                            <div className="mouth"></div>
+                        </div>
+                    </div>
+                    <div className="shadow"></div>
                 </div>
-                <div className="shadow"></div>
-            </div>
-            <div className="bottom">
-                <h4>Boo, looks like a ghost stole this page!</h4>
+                <div className="bottom">
+                    <h4>Boo, looks like a ghost stole this page!</h4>
 
-            </div>
+                </div>
         </>
     );
 }
